8f34a52926847ac088a28b5c340217ad8fade34e
Artin Simons <coder211@163.com>
2020-09-29 09:06:14 +0800

    feat(core): vue demo by cli tool create-vue

---
+++ 04-auto-vue/src/App.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  data() {
+    return {
+      msg: "嗯！ 我喜欢Vue！ "
+    }
+  }
+}
+</script>
+
+<template>
+  <h1 class="msg">{{ msg }}</h1>
+</template>
+
+<style scoped>
+  h1.msg {
+    color: green;
+  }
+</style>

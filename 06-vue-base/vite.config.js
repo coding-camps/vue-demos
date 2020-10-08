db70338ec74e0b53a4eaaf69781ab92f9b5e8171
Artin Simons <coder211@163.com>
2020-10-08 09:57:45 +0800

    feat(core): vue3 compositon api intro - 3

--- 06-vue-base/vite.config.js
+++ 06-vue-base/vite.config.js
@@ -1,10 +1,14 @@
 import { fileURLToPath, URL } from 'node:url'
 
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(
+    // vue 打开对 $ 实验性的自动代理解包 的支持
+    {reactivityTransform:true}
+  )],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))

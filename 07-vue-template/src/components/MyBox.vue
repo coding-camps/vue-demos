0e39bcd07ba9b5186de876c3af4cf44a5dbc4ea9
Artin Simons <coder211@163.com>
2020-10-13 10:24:32 +0800

    feat(core): vue3 style scoped

---
+++ 07-vue-template/src/components/MyBox.vue
@@ -0,0 +1,12 @@
+<script setup></script>
+<template>
+    <div :class="$style.box1">
+        <!-- <h1>MyBox</h1> -->
+        <h2>我是MyBox组件</h2>
+    </div>
+</template>
+<style module>
+.box1 {
+    background-color: orange;
+}
+</style>

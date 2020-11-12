136680a213319f452bc50ee732e2fd0a3bade561
Artin Simons <coder211@163.com>
2020-11-12 09:03:25 +0800

    feat(core): vue3 state intro

---
+++ 11-state/src-v0/components/ComponentB.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { ref } from "vue"
+import ComponentC from "./ComponentC.vue"
+
+const isShow = ref(true)
+</script>
+<template>
+    <h3>ComponentB</h3>
+    <ul>
+        <li v-if="isShow"><ComponentC></ComponentC></li>
+    </ul>
+</template>
---
+++ 11-state/src-v0/components/ComponentC.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref, inject } from "vue"
+
+// const { count, increment } = inject("count")
+// const countStore = inject("count")
+import { countStore } from "../store/count"
+
+
+</script>
+<template>
+    <!-- <h4>
+        ComponentC --  {{ count }} --
+        <button @click="increment">按钮</button>
+    </h4> -->
+    <h4>
+        ComponentC --  {{ countStore.count }} --
+        <button @click="countStore.increment">按钮</button>
+    </h4>
+
+</template>

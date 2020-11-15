5834745242b6f09a50059e553061d58c18c398ca
Artin Simons <coder211@163.com>
2020-11-15 09:24:34 +0800

    feat(exec): create vue3 app about burger shop online

---
+++ 12-meals/src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { useMealsStore } from "./store/meals"
+
+const meals = useMealsStore()
+</script>
+
+<template>
+  <h1><i class="ri-terminal-box-line"></i>汉堡数据</h1>
+  <ol>
+    <li v-for="meal in meals.data">
+      <ol>
+        <li><img :src="meal.img" /></li>
+        <li>id: {{ meal.id }}</li>
+        <li>title: {{ meal.title }}</li>
+        <li>desc: {{ meal.desc }}</li>
+        <li>price: {{ meal.price }}</li>
+      </ol>
+    </li>
+  </ol>
+</template>
+
+<style scoped>
+</style>

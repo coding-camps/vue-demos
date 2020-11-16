201850da17da7843a3c1720232d331a812682345
Artin Simons <coder211@163.com>
2020-11-16 10:33:28 +0800

    feat(exec): Add components - MealList, Meal, Counter

---
+++ 12-meals/src/components/meals/MealList.vue
@@ -0,0 +1,20 @@
+<script setup>
+import {useMealsStore} from "@/store/meals"
+import Meal from "@/components/meals/Meal.vue"
+
+const meals = useMealsStore()
+</script>
+
+<template>
+    <div class="meals">
+        <Meal v-for="meal in meals.data" :meal="meal"></Meal>
+    </div>
+</template>
+
+<style scoped>
+.meals {
+    height: 100vh;
+    padding: 120rem 0;
+    overflow: auto;
+}
+</style>
---
+++ 12-meals/src/components/ui/Counter.vue
@@ -0,0 +1,44 @@
+<script setup>
+import {ref} from "vue";
+
+const count = ref(0)
+</script>
+
+<template>
+    <div class="counter">
+        <template v-if="count>0">
+            <button class="sub" @click="count--"><i class="ri-subtract-line"></i></button>
+            <span>{{ count }}</span>
+        </template>
+        <button class="add" @click="count++"><i class="ri-add-line"></i></button>
+    </div>
+</template>
+
+<style scoped>
+.counter {
+    display: flex;
+}
+
+.counter span {
+    font-size: 30rem;
+    margin: 0 10rem;
+}
+
+.sub, .add {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 34rem;
+    height: 36rem;
+    background-color: rgb(234, 178, 0);
+    border: none;
+    border-radius: 50%;
+    font-size: 36rem;
+}
+
+.sub {
+    background-color: white;
+    border: 1px solid white;
+}
+
+</style>
